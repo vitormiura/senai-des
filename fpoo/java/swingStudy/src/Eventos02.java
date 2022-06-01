eef0a145881a8160998a1d680d2706d60d5ba8e3
vitormiura <vitormiura3@gmail.com>
2022-06-01 11:30:23 -0300

    update somativa java

--- fpoo/java/swingStudy/src/Eventos02.java
+++ fpoo/java/swingStudy/src/Eventos02.java
@@ -2,13 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Eventos02 extends JFrame{
+public class Eventos02 extends JFrame {
 
     JTextField login;
     JPasswordField senha;
     JButton ok, cancel;
-    
-    public Eventos02(){
+
+    public Eventos02() {
         super("Login - Orkut 2.0");
 
         login = new JTextField();
@@ -19,32 +19,34 @@ public class Eventos02 extends JFrame{
         cancel.addActionListener(new BotaoCancelListener());
 
         Container c = getContentPane();
-        c.setLayout(new GridLayout(3,2));
+        c.setLayout(new GridLayout(3, 2));
         c.add(new JLabel("Login:"));
         c.add(login);
         c.add(new JLabel("Senha:"));
         c.add(senha);
         c.add(ok);
         c.add(cancel);
-        
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300,300);
+        setSize(300, 300);
         setVisible(true);
     }
+
     public static void main(String[] args) {
         new Eventos02();
     }
 
-    class BotaoOkListener implements ActionListener{
+    class BotaoOkListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
-            String s = "Login: " +login.getText()+"\nSenha: "+new String(senha.getPassword());
+        public void actionPerformed(ActionEvent e) {
+            String s = "Login: " + login.getText() + "\nSenha: " + new String(senha.getPassword());
             JOptionPane.showMessageDialog(null, s);
         }
     }
-    class BotaoCancelListener implements ActionListener{
+
+    class BotaoCancelListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e){
+        public void actionPerformed(ActionEvent e) {
             login.setText("");
             senha.setText("");
             JOptionPane.showMessageDialog(null, "Dados limpos com Sucesso!");
