8e7972aa44c111d92314d39bc6a8feeed43f3afd
vitormiura <vitormiura3@gmail.com>
2022-07-22 10:46:03 -0300

    change fields in models to test django auto fix

--- home/admin.py
+++ home/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from home.models import Agenda
 
 class detailAgenda(admin.ModelAdmin):
-    list_display =  ('id','name', 'date', 'is_active')
-    list_editable = ('is_active',)
+    list_display =  ('id','name', 'date', 'teste','price')
+    list_editable = ('teste',)
     list_per_page = 15
     search_fields = ('name',)
     list_display_links = ('name',)
--- home/models.py
+++ home/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Agenda(models.Model):
     name = models.CharField(max_length=66)
     description = models.CharField(max_length=255)
-    is_active = models.BooleanField()
+    #is_active = models.BooleanField()
+    teste = models.BooleanField()
     date = models.DateField(auto_now_add=True)
     email = models.EmailField()
     price = models.DecimalField(max_digits=50, decimal_places=2)
