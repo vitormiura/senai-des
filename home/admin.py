1e1892138d4c4c841d140d0fea9399aa9d7964ec
vitormiura <vitormiura3@gmail.com>
2022-07-22 10:14:21 -0300

    add first table and migrate to db

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from home.models import Agenda
+
+class detailAgenda(admin.ModelAdmin):
+    list_display =  ('id','name', 'date', 'is_active')
+    list_editable = ('is_active',)
+    list_per_page = 15
+    search_fields = ('name',)
+    list_display_links = ('name',)
+
+admin.site.register(Agenda, detailAgenda)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Agenda(models.Model):
+    name = models.CharField(max_length=66)
+    description = models.CharField(max_length=255)
+    is_active = models.BooleanField()
+    date = models.DateField(auto_now_add=True)
+    email = models.EmailField()
+    price = models.DecimalField(max_digits=50, decimal_places=2)
+
+    def __str__(self):
+        return self.name
