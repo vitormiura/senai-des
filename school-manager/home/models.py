2a14efcd6fb6c93eaed0d5659053940c7d711aef
vitormiura <vitormiura3@gmail.com>
2022-07-27 08:51:50 -0300

    add school init table

--- school-manager/home/models.py
+++ school-manager/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nome = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    active = models.BooleanField()
