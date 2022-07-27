09412ccd270b30603c28236a1a96250e1850febd
vitormiura <vitormiura3@gmail.com>
2022-07-27 11:26:44 -0300

    tranform to "components" html parts

--- school-manager/home/views.py
+++ school-manager/home/views.py
@@ -7,10 +7,11 @@ from home.models import Curso
 def home_index(request):
     dados = Curso.objects.all() 
     context={
-        'curso': dados
+        'curso' : dados
     }
 
     return render(request, 'home/index.html', context)
 
 def home_detalhes(request, id):
-    return HttpResponse(id)
+    data = Curso.objects.get(id = id)
+    return render(request, 'home/detalhes.html', {'curso' : data})
