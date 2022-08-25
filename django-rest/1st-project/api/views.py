264e6aadd0637c24475107b5598bff376d45f85e
vitormiura <vitormiura3@gmail.com>
2022-08-25 08:50:30 -0300

    Setup views/serializers

---
+++ django-rest/1st-project/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getData(request):
+    person = {
+        'name':'Doe',
+        'age':'69'
+    }
+    return Response(person)
