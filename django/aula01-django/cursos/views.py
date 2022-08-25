512e6b2685468b9b4226b0a39d8344aa03624c7a
vitormiura <vitormiura3@gmail.com>
2022-08-25 16:55:24 -0300

    Add base and add courses to views.

--- django/aula01-django/cursos/views.py
+++ django/aula01-django/cursos/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# def index(request):
+#     return render(request, 'index.html')
+
 def index(request):
-    return render(request, 'index.html')
+    cursos = {
+        '1':'Smart',
+        '2':'Manufatura',
+        '3':'Mecatrônica',
+        '4':'Adm'
+    }
+
+    dados = {
+        'nome_cursos' : cursos
+    }
+
+    return render(request, 'index.html', dados)
 
 def cursos(request):
     return render(request, 'curso.html')
