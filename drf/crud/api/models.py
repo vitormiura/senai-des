b549f8cac027de2fa9605aff961c80a1572b1f25
vitormiura <vitormiura3@gmail.com>
2022-08-26 10:23:02 -0300

    Add models and admin

--- drf/crud/api/models.py
+++ drf/crud/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    locationName = models.CharField(max_length=111, unique=True)
+
+    def __str__(self):
+        return self.locationName
+
+class Item(models.Model):
+    itemName = models.CharField(max_length=100)
+    date_added = models.DateField(auto_now_add=True)
+    itemLocation = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.itemName
