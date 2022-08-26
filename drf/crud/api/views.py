09e682627c4547a803d22b6df51fe894dfe0772b
vitormiura <vitormiura3@gmail.com>
2022-08-26 11:08:58 -0300

    Add views

--- drf/crud/api/views.py
+++ drf/crud/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from pydoc import locate
+from rest_framework import generics
+from .models import Item, Location
+from .serializer import ItemSerializer, LocationSerializer
 
-# Create your views here.
+class ItemList(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        location = self.request.query_params.get('location')
+
+        if location is not None:
+            queryset = queryset.filter(itemLocation = location)
+        return queryset
+
+class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+class LocationList(generics.ListCreateAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
