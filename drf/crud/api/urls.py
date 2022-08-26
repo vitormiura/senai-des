ca77a4f819cfd83bb9de63ec09157ed4a7a10aec
vitormiura <vitormiura3@gmail.com>
2022-08-26 10:29:31 -0300

    Add views and serializers

---
+++ drf/crud/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ItemList, ItemDetail, LocationList, LocationDetail
+
+urlpatterns = [
+    path('item/', ItemList.as_view()),
+    path('item/<int:pk>/', ItemDetail.as_view()),
+    path('location/', LocationList.as_view()),
+    path('location/<int:pk>/', LocationDetail.as_view()),
+]
