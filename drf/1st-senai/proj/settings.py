8783a6c71bd46643c5b0c647b0b0c6c55f63431c
vitormiura <vitormiura3@gmail.com>
2022-08-30 08:13:52 -0300

    Init new app

--- drf/1st-senai/proj/settings.py
+++ drf/1st-senai/proj/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # apps
+    'loja',
+    # libs
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
