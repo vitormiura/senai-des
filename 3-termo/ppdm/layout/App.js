ff95d124726af6ac336c8d5df88e614340ae8860
vitormiura <vitormiura3@gmail.com>
2023-02-02 11:16:12 -0300

    ppdm(layout): add orientation and dimension examples

--- 3-termo/ppdm/layout/App.js
+++ 3-termo/ppdm/layout/App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+  const posicao = useDeviceOrientation();
+  console.log(posicao);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={{ height: posicao == "landscape" ? "100%" : "100%" }}>
+        <View
+          style={{
+            backgroundColor: "red",
+            flex: posicao == "landscape" ? 1 : 0.87,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: "green",
+            flex: 1,
+          }}
+        />
+        <Button title="butao" onPress={() => alert("jaja", 'haha')}/>
+        <View
+          style={{
+            backgroundColor: "black",
+            flex: 1,
+          }}
+        />
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
